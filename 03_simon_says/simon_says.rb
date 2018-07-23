b3fb5507422a86b4212300a5907afa5115fa7721
Ross Parkhill <rossrtw2010@gmail.com>
2018-07-23 21:36:13 +0700

    00 & 03 done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
-#write your code here
+def echo(str)
+  str
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, n=2)
+  (" #{str}" * n).lstrip
+end
+
+def start_of_word(str, lposition)
+  str[0, lposition]
+end
+
+def first_word(str)
+  str.split(" ").first
+  #str.lines(" ").to_a.first.rstrip   // string#to_a removed > v1.9
+end
+
+def titleize(str)
+  little_words = ["and", "the", 'over']
+  arr = str.split(" ")
+  for word in arr do
+    word.capitalize! unless little_words.include?(word) && arr.index(word) != 0
+  end
+  arr.join(" ")
+end
