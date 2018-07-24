51f8a8b91497fe4c886a862dc6c5c61704342dfd
Ross Parkhill <rossrtw2010@gmail.com>
2018-07-24 22:35:08 +0700

    Calculator complete

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(x, y)
+  x+y
+end
+
+def subtract(x, y)
+  x-y
+end
+
+def sum(arr)
+  !arr.empty? ? arr.reduce(:+) : 0
+end
+
+def multiply(arr)
+  arr.reduce(:*)
+end
+
+def power(b,e)
+  b**e
+end
+
+def factorial(num)
+  num === 0 ? 1 : num *= factorial(num-1)
+end
