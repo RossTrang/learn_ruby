18e501d9c642cec9671b15554a407e34a3c701eb
Ross Parkhill <rossrtw2010@gmail.com>
2018-07-25 00:08:23 +0700

    All complete

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+
+  def time_string
+    t = Time.utc(2018) + @seconds
+    t.strftime("%T")
+  end
+
 end
