717e235669451d0fe2f93b249c328265126aed48
Ross Parkhill <rossrtw2010@gmail.com>
2018-07-24 21:25:29 +0700

    Book Titles complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
-# write your code here
+
+  attr_accessor :title
+
+  def title
+    little_words = %w[a an and the of in]
+    @title.split.each_with_index.map { |word, index| little_words.include?(word) && index != 0 ? word : word.capitalize}.join(" ")
+  end
+
 end
